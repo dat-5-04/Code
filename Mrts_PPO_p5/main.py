9588f3ccb4b533073084ea33487c10cdcbe2f2b7
ZakariaB99 <91832475+ZakariaB99@users.noreply.github.com>
2023-11-04 11:40:18 +0100

    Update main.py

--- Mrts_PPO_p5/main.py
+++ Mrts_PPO_p5/main.py
@@ -6,11 +6,8 @@ import time
 from distutils.util import strtobool
 from typing import List
 
-import numpy as np
-import pandas as pd
-import torch
-import torch.nn as nn
 import torch.optim as optim
+from gym_microrts import microrts_ai
 
 from torch.utils.tensorboard import SummaryWriter
 
@@ -29,15 +26,22 @@ if __name__ == "__main__":
     #Everything optional for the train
     experiment_name = f"{args.gym_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
     device = rtsUtils.getTorchDevice(args) #get device
-    envs = envInitializer.envInitializer(args=args)
+    
+    envs = envInitializer.envInitializer(args=args, ai2s = 
+          [microrts_ai.coacAI for _ in range(args.num_bot_envs - 6)]
+        + [microrts_ai.randomBiasedAI for _ in range(min(args.num_bot_envs, 2))]
+        + [microrts_ai.lightRushAI for _ in range(min(args.num_bot_envs, 2))]
+        + [microrts_ai.workerRushAI for _ in range(min(args.num_bot_envs, 2))]
+    )
+    
     agent = agentSetup.Agent(envs).to(device)
     optimizer = optim.Adam(agent.parameters(), lr=args.learning_rate, eps=1e-5)
 
     writer = SummaryWriter(f"log/{experiment_name}")
     writer.add_text("hyperparameters", "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])))
 
-    model = maskedPPO.PPOTrainer(args,envs,agent,writer,optimizer,device,"modelNameHere")
+    model = maskedPPO.PPOTrainer(args,envs,agent,writer,optimizer,device)
     model.train()
-    
+
     envs.close()
     writer.close()
